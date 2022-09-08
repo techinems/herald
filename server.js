3b66d352a1228758f9ee0dd95137fcec640bb70c
Dan Bruce <dan@ddbruce.com>
2022-09-07 20:03:28 -0400

    remove commented non-working code

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
-// const { fs } = require("fs");
-
 const SMTPServer = require("simple-smtp-listener").Server;
 const simpleParser = require("mailparser").simpleParser;
-
-// const server = new SMTPServer({
-//   // secure: true,
-//   // key: fs.readFileSync("private.key"),
-//   // cert: fs.readFileSync("server.crt")
-//   onData(stream, session, callback) {
-//     stream.pipe(process.stdout);
-//     stream.on("end", callback);
-//   },
-// });
-
-// server.listen(587);
 
 const server = new SMTPServer(25);
 server.on("tmd@herald.lp13.rpiambulance.com", async (mail) => {
   console.log("text: " + mail.text);
-  //   let parsed = await simpleParser(mail);
-  //   console.log(parsed);
-  //   console.log("got a meesage");
 });
 
 console.log("up");
