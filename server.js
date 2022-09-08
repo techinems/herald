c31d5278456d9ec9d72bfea58be62b3bb9274cbc
Dan Bruce <dan@ddbruce.com>
2022-09-07 21:02:06 -0400

    work on dispatch string splitting

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const SMTPServer = require("simple-smtp-listener").Server;
-const simpleParser = require("mailparser").simpleParser;
+
+require("dotenv").config();
+
+const FIELDS = JSON.parse(process.env.FIELDS);
+
+const createRegex = () => {
+  let regexString = "\\s*(";
+  for (const field of FIELDS) {
+    regexString += field;
+    regexString += ")\\s*|\\s*(";
+  }
+  regexString = regexString.slice(0, -5);
+  console.log(regexString);
+  const regex = new RegExp(regexString);
+  return regex;
+};
+
+const handleMessage = async (mail) => {
+  let data = [];
+};
 
 const server = new SMTPServer(25);
 server.on("tmd@herald.lp13.rpiambulance.com", async (mail) => {
-  console.log("text: " + mail.text);
+  handleMessage(await mail);
 });
 
 console.log("up");
+
+console.log(createRegex());
--- test_client.js
+++ test_client.js
@@ -18,7 +18,7 @@ var message = {
   from: "test@localhost",
   to: "tmd@herald.lp13.rpiambulance.com",
   subject: "cadpaging",
-  text: "PAGE SENT TO RE59 INCIDENT 22-184838 CALL TYPE B-Falls ADDRESS 1649 15th Street APT / FLR LOCATION  RPI - Russel Sage Dining Hall (RPI) CROSS STREETS  Bouton Road/Sage Avenue EMD CODE 17B04 LATITUDE 42729787 LONGITUDE 73678242",
+  text: "PAGE SENT TO RE59 INCIDENT 22-184838 CALL TYPE B-Falls ADDRESS 1649 15th Street APT / FLR LOCATION RPI - Russel Sage Dining Hall (RPI) CROSS STREETS Bouton Road/Sage Avenue EMD CODE 17B04 LATITUDE 42729787 LONGITUDE 73678242",
 };
 
 transporter.sendMail(message);
